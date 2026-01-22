4ac76dfa9ff409ded53b96544e49d1d1821d733b
Rushabh Rathod <rathodrhushabh@gmail.com>
2026-01-22 19:49:49 +0530

    cors update

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: "school-book-inventory-frontend-g1fh.vercel.app"
 }));
 
 app.use("/book-set", bookSetRoutes);
